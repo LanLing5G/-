46761798aadc2aee4a361587f25a46e9331d05f6
tydd <ty_1487@163.com>
2021-06-21 15:44:14 +0800

    第二次提交 2021/6/21

---
+++ com/neu/ch1/TestScope.java
@@ -0,0 +1,17 @@
+package com.neu.ch1;
+
+public class TestScope {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		int x=12;
+		//{
+			int q=96;
+			System.out.println("x is"+x);
+			System.out.println("q is"+q);
+		//}
+		q=x;
+		System.out.println("x is"+x);
+	}
+
+}
