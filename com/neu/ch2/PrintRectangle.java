912a09455b346b0ae9c7ae35c099960fd752e253
tydd <ty_1487@163.com>
2021-06-22 09:18:00 +0800

    第三次提交 2021/6/22

---
+++ com/neu/ch2/PrintRectangle.java
@@ -0,0 +1,26 @@
+package com.neu.ch2;
+
+
+
+public class PrintRectangle {
+	
+	public static void drawRectangle(int x,int y) {
+		for(int i=0;i<x;i++) {
+			for(int j=0;j<y;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+	
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		drawRectangle(3,5);
+		drawRectangle(2,4);
+		drawRectangle(6,10);
+	}
+
+}
+
+
